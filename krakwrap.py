426086758508dc8d5f9ec8018ab1859874eff459
Gabriel Firestone <gfirest@ncsu.edu>
2018-01-02 02:35:23 -0500

    handles the python exceptions that krakenex most obviously throws, HTTPError and key-secret loading errors

--- krakwrap.py
+++ krakwrap.py
@@ -1,6 +1,8 @@
 #
 # This program extends the Krakenex library by veox (github.com/veox/python3-krakenex): 
 # and hopefully generalizes it a bit
+#   It takes it from a class to an object (kinda useless but it kinda helps keep track if we have
+#   multiple instances running
 #   It does some minor error checking:
 #     if requests library returns a 5xx http error, it retries a few times (5?)
 #
@@ -22,6 +24,7 @@ import krakenex
 #payload = {'aclass':'currency','asset':'ZUSD,XXDG'}
 #r = requests.post(baseurl+urltext, payload)
 #r.text
+#'{"error":["EGeneral:Invalid arguments"]}'
 
 class wrappedAPI() :
   def __init__(self):
@@ -38,11 +41,33 @@ class wrappedAPI() :
   def query_public(self, method, data=None, count=5):
     try:
       self.resultRaw = self.api.query_public(method, data)
-    except HTTPError
-      pass
+    except requests.exceptions.HTTPError:
+      # kraken has a 520 error problem right now, try it 5 times then give up
+      if count > 0:
+        time.sleep(1)
+        self.query_public(method, data, count-1 )
+      else:
+        raise
     
   def query_private(self, method, data=None):
-    self.resultRaw = self.api.query_private(method, data)
+    try:
+      self.resultRaw = self.api.query_private(method, data)
+    except requests.exceptions.HTTPError:
+      # kraken has a 520 error problem right now, try it 5 times then give up
+      if count > 0:
+        time.sleep(1)
+        self.query_public(method, data, count-1 )
+      else:
+        raise
+    except Exception as e:
+      #the krakenex library is a little weird on this error, not sure if i want to deal with it at all
+      if e.args[0] == "Either key or secret is not set! (Use `load_key()`.":
+        print("Key/Secret Error, make sure you load the correct file or use query_public")
+        # kick the can to get this caught in the error checking routine
+        return {'error': ["EGeneral:Invalid arguments"], 'result': {}}
+      else:
+        raise
+
 
 if __name__ == "__main__":
   
